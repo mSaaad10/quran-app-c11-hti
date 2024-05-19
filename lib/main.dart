03a5d598397c85901a022d22838affe4179a6c4e
mSaad <muhammedsaad.route@gmail.com>
2024-05-19 17:15:44 +0300

    % app class name

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/ui/home/home_screen.dart';
 import 'package:quran_app/ui/home/chapter_details_screen/chapter_details_screen.dart';
+import 'package:quran_app/ui/home/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApppp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApppp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
